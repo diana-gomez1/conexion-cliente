9bd4a01b8a9ac032cc00edc305b6202696a2f66b
Diana Gomez <dianagomez@test.denkitronik.com>
2023-02-20 15:23:47 -0500

    agreg

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@
  * 
 */
 void setup() {
- pinMode(2,OUTPUT); //Coloco el pi 2 coo salida
+ pinMode(2,OUTPUT); //Coloco el pin 2 como salida
 }
 /**
- * @brief Esta fcio co bcle ifiito
+ * @brief Esta funcion con bucle infinito
 */
 void loop() {
-  digitalWrite(2,HIGH);
-  delay(500);
-  digitalWrite(2,LOW);
-  delay(500);
+  digitalWrite(2,HIGH);//Led enciede
+  delay(500);//espero 500ms
+  digitalWrite(2,LOW);//apago led
+  delay(500);//espero 500ms
 }
 
