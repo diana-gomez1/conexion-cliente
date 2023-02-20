c8aa5ff68ba8d8ec409a6b14d66871bac2f48ede
Diana Gomez <dianagomez@test.denkitronik.com>
2023-02-20 15:09:35 -0500

    Agregue codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
-
+/**
+ * @brief Esta es la 
+ * 
+ * 
+*/
 void setup() {
-  // put your setup code here, to run once:
+ pinMode(2,OUTPUT); //Coloco el pi 2 coo salida
+}
+/**
+ * @brief Esta fcio co bcle ifiito
+*/
+void loop() {
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
