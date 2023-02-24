d0340b0f5f1d688b1ac6aa5eda6b571a15654a57
Diana Gomez <dianagomez@test.denkitronik.com>
2023-02-24 17:36:45 -0500

    Se agrego el codigo de conexion a Wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <Arduino.h>
+#include <WiFi.h>
+const char * ssid = "virus";
+const char * password = "elec1929";
+ 
+
+
+
 /**
- * @brief Esta es la 
+ * @brief Esta es la iioo
  * 
  * 
 */
+
 void setup() {
  pinMode(2,OUTPUT); //Coloco el pin 2 como salida
+ Serial.begin(115200);
+ Serial.println("Inicializando el dispositivo");
+ WiFi.begin(ssid, password);
+
+ Serial.println("Estableciendo vinculo con el AP.");
+ while (WiFi.status() != WL_CONNECTED)
+ {
+  Serial.print(".");
+  delay(1000);
+ }
+ Serial.println("\r\n Conexion establecida");
+ 
 }
 /**
  * @brief Esta funcion con bucle infinito
