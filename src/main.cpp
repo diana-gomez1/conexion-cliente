dfec8978eeadd5b25c9af83c032bf7ff7426a963
Diana Carolina Gomez <dianaCdenkitronik.com>
2023-03-27 10:15:21 -0500

    cliente

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <WiFi.h>
-const char * ssid = "virus";
-const char * password = "elec1929";
+const char * ssid = "ASUSDCGG";
+const char * password = "3164173706Cc";
+const char * host = "www.google.com";
+const int puerto = 80;
  
 
 
@@ -25,16 +27,44 @@ void setup() {
   delay(1000);
  }
  Serial.println("\r\n Conexion establecida");
- 
+ Serial.print("Numero Ip asignado:");
+ Serial.println(WiFi.localIP());//imprime numero ip 
 }
 /**
  * @brief Esta funcion con bucle infinito
  prueba
 */
 void loop() {
-  digitalWrite(2,HIGH);//Led enciede
-  delay(500);//espero 500ms
-  digitalWrite(2,LOW);//apago led
-  delay(500);//espero 500ms
+  WiFiClient cliente;//creamos cliente TCP por wifi 
+  
+  //BLOQUE que raliza la conexion al servidor
+  if(!cliente.connect(host, puerto)){
+    Serial.println("Error conexion al host fallida");
+    delay(2000);
+    return;
+  } 
+
+  //Peticion (request) GET al servidor HTTP
+  cliente.print("GET /index.html HTTP/1.1\r\nHost. "+String(host)+"\r\n"+"Connection: close\r\n\r\n");
+  //Tiempo al servidor a que responda la peticion(response)
+  //delay(5000);// no funciona aqui porque bloquea wifi 
+  unsigned long milisegundos = millis();//hora de inicio
+  while (cliente.available()== 0){//preguntamos si no hay datos recibidos disponibles
+      if(millis()-milisegundos > 5000){//millis hora actual  menos el instante anterior
+      Serial.println("Se expiro el tiempo de la conexion");
+      cliente.stop();
+      }
+   
+   }
+  while (cliente.available()){
+    String linea = cliente.readStringUntil('\r');//lea string hasta que encuentre lo que esta en comilla simple
+    Serial.println(linea);
+  }
+    Serial.println("Fin de conexion");
+    cliente.stop();
+  
+  
+    
+    delay(2000);//espero 2s
 }
 
